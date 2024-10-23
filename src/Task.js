9979007f7f89edb8175589a976056295e1f08959
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-10-23 17:31:41 +0530

    applied state hooks

--- src/Task.js
+++ src/Task.js
@@ -1,34 +1,59 @@
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
 
 const Task = () => {
+    const [task, setTask] = useState("");
+
+    const [taskList, setTaskList] = useState([]);
+
+    // handle input values
+
+    const handleInputValue = (input) => {
+        setTask(input);
+    };
+
+    //  handling button funcntioning
+
+    const handleAddButton = () => {
+        setTaskList((prevList) => {
+            return [...prevList, task];
+            alert("task Added");
+            setTask('');
+        });
+    };
     return (
         <View style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput style={styles.inputBox} placeholder='Add Your Tasks' />
-                <Button title='Add your task' />
+                <TextInput
+                    onChangeText={handleInputValue}
+                    style={styles.inputBox}
+                    placeholder="Add Your Tasks"
+                />
+                <Button onPress={handleAddButton} title="Add your task" />
             </View>
             <View>
                 <Text style={styles.border}></Text>
                 <Text style={styles.textTitle}>Your Tasks : </Text>
+                <View >
+                    {
+                        taskList.map((item, index) => (<Text style={styles.taskItem} key={index}>{item}</Text>))
+                    }
+                </View>
 
             </View>
         </View>
-    )
-}
-
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         paddingTop: 50,
-        paddingHorizontal: 15
-
+        paddingHorizontal: 15,
     },
     inputContainer: {
         flexDirection: "row",
         justifyContent: "space-between",
-        alignItems: "center"
-
+        alignItems: "center",
     },
     inputBox: {
         width: "60%",
@@ -36,23 +61,25 @@ const styles = StyleSheet.create({
         borderColor: "#cccCCC",
         borderRadius: 10,
         padding: 5,
-        paddingLeft: 15
-
-
+        paddingLeft: 15,
     },
-    textTitle:
-    {
+    textTitle: {
         fontSize: 26,
         fontWeight: "bold",
         marginTop: 10,
-        color: "blue"
-
+        color: "blue",
     },
-    border:
-    {
+    border: {
         borderBottomWidth: 1,
-        color: "#cccccc"
+        color: "#cccccc",
+    },
+    taskItem:
+    {
+        margin: 10,
+        padding: 20,
+        borderRadius: 20,
+        backgroundColor: "green",
+        color: "white"
     }
-
-})
-export default Task
+});
+export default Task;
